b0728915fc9a49dc1c490077d682aa60060be03d
LeeDongHoon <likelion.leedh@gmail.com>
2026-01-10 17:24:48 +0900

    init

---
+++ src/main/java/com/example/short_kki/ShortKkiApplication.java
@@ -0,0 +1,13 @@
+package com.example.short_kki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortKkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortKkiApplication.class, args);
+	}
+
+}
