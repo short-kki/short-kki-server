02842667b31f97721db798a22c2c2c78e3fd3f4f
Lee ChanMi <anytime0224@gmail.com>
2026-01-15 15:11:02 +0900

    fix: 기본 ShortKkiApplicationTests도 통합 테스트 클래스 상속

---
+++ src/test/java/com/example/short_kki/test/smoke/ShortKkiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.short_kki.test.smoke;
+
+import com.example.short_kki.test.support.IntegrationTestBase;
+import org.junit.jupiter.api.Test;
+
+class ShortKkiApplicationTests extends IntegrationTestBase {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
