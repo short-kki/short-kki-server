a058a008ad84a3f7959f0b9e95542aebc8503fa4
Lee ChanMi <anytime0224@gmail.com>
2026-01-24 05:23:32 +0900

    feat: File upload 요청 값에 validation 추가 (#18)

--- src/main/java/com/example/short_kki/domain/file/controller/dto/FileUploadRequest.java
+++ src/main/java/com/example/short_kki/domain/file/controller/dto/FileUploadRequest.java
@@ -2,15 +2,27 @@ package com.example.short_kki.domain.file.controller.dto;
 
 import com.example.short_kki.domain.file.entity.FileTargetType;
 import com.example.short_kki.domain.file.entity.FileVisibility;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 
 @Builder
 public record FileUploadRequest(
+
+        @NotBlank(message = "filename은 필수입니다.")
         String filename,
+
+        @NotBlank(message = "contentType은 필수입니다.")
         String contentType,
+
+        @Positive(message = "contentLength는 0보다 커야 합니다.")
         long contentLength,
+
+        @NotNull(message = "visibility는 필수입니다.")
         FileVisibility visibility,
+
+        @NotNull(message = "targetType은 필수입니다.")
         FileTargetType targetType
 ) {
-
 }
