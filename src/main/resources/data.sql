008ca54ff7dcc027c959776d63b02db9a2965b9f
Lee ChanMi <anytime0224@gmail.com>
2026-01-24 05:07:04 +0900

    chore: 유저 테스트 데이터 추가 (#18)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO member (email, name, oauth_id, oauth_provider, role, created_at, updated_at)
+VALUES ('test@example.com', '테스트유저', 'test-oauth-id-001', 'GOOGLE', 'USER', NOW(6), NOW(6));
