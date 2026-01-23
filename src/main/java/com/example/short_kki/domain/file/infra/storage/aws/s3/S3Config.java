5dc2e33749656e704a68d86be023edd3a196b422
Lee ChanMi <anytime0224@gmail.com>
2026-01-24 05:01:28 +0900

    feat: aws sdk 2 및 s3 의존성 추가 (#18)

---
+++ src/main/java/com/example/short_kki/domain/file/infra/storage/aws/s3/S3Config.java
@@ -0,0 +1,45 @@
+package com.example.short_kki.domain.file.infra.storage.aws.s3;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@Configuration
+@RequiredArgsConstructor
+public class S3Config {
+
+    @Bean
+    @Profile("!local")
+    public S3Presigner s3Presigner(@Value("${aws.region}") String region) {
+        return S3Presigner.builder()
+                .region(Region.of(region))
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+
+    // TODO: 개발 서버 올라가면 삭제
+    @Bean
+    @Profile("local")
+    public S3Presigner localS3Presigner(
+            @Value("${aws.region}") String region,
+            @Value("${aws.service.s3.credentials.access-key}") String accessKey,
+            @Value("${aws.service.s3.credentials.secret-key}") String secretKey
+    ) {
+        return S3Presigner.builder()
+                .region(Region.of(region))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(accessKey, secretKey)
+                        )
+                )
+                .build();
+    }
+}
+
