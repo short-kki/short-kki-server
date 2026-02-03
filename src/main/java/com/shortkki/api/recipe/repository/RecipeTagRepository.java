7fa9f6b37e005cc99d53856037556f789f7582ba
theeunseojo <ces80372@naver.com>
2026-02-04 02:44:02 +0900

    feat : 레시피 조회시 태그 포함  (#37) - 태그 조회 쿼리 최적화 - 레시피 태그 로직 추가

--- src/main/java/com/shortkki/api/recipe/repository/RecipeTagRepository.java
+++ src/main/java/com/shortkki/api/recipe/repository/RecipeTagRepository.java
@@ -3,10 +3,20 @@ package com.shortkki.api.recipe.repository;
 import com.shortkki.api.recipe.entity.RecipeTag;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RecipeTagRepository extends JpaRepository<RecipeTag, Long> {
 
     List<RecipeTag> findByRecipeId(Long recipeId);
 
     void deleteByRecipeId(Long recipeId);
+
+    @Query("""
+                select t.name
+                from RecipeTag rt
+                join Tag t on t.id = rt.tagId
+                where rt.recipeId = :recipeId
+            """)
+    List<String> findTagNamesByRecipeId(@Param("recipeId") Long recipeId);
 }
