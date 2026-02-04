6a338d046a29ddafc3dcb75aa8b0c0ccd732eed8
theeunseojo <ces80372@naver.com>
2026-02-04 12:51:58 +0900

    refactor : 타임아웃 설정 필요할때 사용하도록 설정(#46)

--- src/main/java/com/shortkki/global/config/RestClientConfig.java
+++ src/main/java/com/shortkki/global/config/RestClientConfig.java
@@ -2,28 +2,27 @@ package com.shortkki.global.config;
 
 import java.time.Duration;
 import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
+import org.springframework.boot.http.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
 
     @Bean
-    public RestClient restClient(
-            ClientHttpRequestFactoryBuilder<?> clientHttpRequestFactoryBuilder) {
-        return RestClient.builder()
-                .requestFactory(clientHttpRequestFactory())
-                .build();
-    }
+    public RestClient restClient(RestClient.Builder builder) {
+        var settings = new ClientHttpRequestFactorySettings(
+                ClientHttpRequestFactorySettings.Redirects.FOLLOW_WHEN_POSSIBLE,
+                Duration.ofSeconds(5),
+                Duration.ofSeconds(10),
+                null
+        );
 
-    // 타임아웃 설정
-    private ClientHttpRequestFactory clientHttpRequestFactory() {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(Duration.ofSeconds(5));
-        factory.setReadTimeout(Duration.ofSeconds(10));
-        return factory;
+        var requestFactory = ClientHttpRequestFactoryBuilder
+                .detect()
+                .build(settings);
+
+        return builder.requestFactory(requestFactory).build();
     }
 }
