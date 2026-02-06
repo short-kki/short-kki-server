65c48bf8a48508fff9e4991e5a7a644dee9c33c3
Lee ChanMi <anytime0224@gmail.com>
2026-02-06 15:07:47 +0900

    feat: 멤버에 프로필 이미지 추가

--- src/main/java/com/shortkki/api/member/entity/Member.java
+++ src/main/java/com/shortkki/api/member/entity/Member.java
@@ -1,23 +1,30 @@
 package com.shortkki.api.member.entity;
 
+import com.shortkki.api.file.entity.FileMetadata;
 import com.shortkki.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "member")
-@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
 public class Member extends BaseEntity {
 
     @Id
@@ -41,24 +48,21 @@ public class Member extends BaseEntity {
     @Column(nullable = false)
     private Role role;
 
-    @Builder
-    private Member(String email, String name, String oauthId, OAuthProvider oauthProvider,
-            Role role) {
-        this.email = email;
-        this.name = name;
-        this.oauthId = oauthId;
-        this.oauthProvider = oauthProvider;
-        this.role = role;
-    }
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "profile_img_file_id")
+    private FileMetadata profileImgFile;
 
-    public static Member create(String email, String name, String oauthId,
-            OAuthProvider oauthProvider) {
+    public static Member create(
+            String email, String name, String oauthId, OAuthProvider oauthProvider,
+            FileMetadata profileImgFile
+    ) {
         return Member.builder()
                 .email(email)
                 .name(name)
                 .oauthId(oauthId)
                 .oauthProvider(oauthProvider)
                 .role(Role.USER)
+                .profileImgFile(profileImgFile)
                 .build();
     }
 
@@ -69,5 +73,9 @@ public class Member extends BaseEntity {
     public void updateOAuthInfo(String oauthId, OAuthProvider oauthProvider) {
         this.oauthId = oauthId;
         this.oauthProvider = oauthProvider;
+    }
+
+    public String getProfileImgUrl() {
+        return profileImgFile != null ? profileImgFile.getUrl() : null;
     }
 }
